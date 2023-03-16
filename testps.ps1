90b0c79240689c6f7a8eea272899dc29da7a343b
Antony Chiu <chiu_antony@hcl.com>
2023-03-17 01:10:33 +0800

    err

--- testps.ps1
+++ testps.ps1
@@ -5,4 +5,6 @@ $env:TESTENVVAR = 'value of a test env var'
 for($i=0;$i -le 60;$i++){
     Write-Host "This is a sample PS script: $i"
     Start-Sleep -Seconds 1
+
+    Write-Error "Write error message"
 }

c00274a5c9814839e6a1312b0dbfed8980ad871b
Antony Chiu <chiu_antony@hcl.com>
2023-03-17 00:14:43 +0800

    test

--- callps.js
+++ callps.js
@@ -1,10 +1,11 @@
 var spawn = require("child_process").spawn,child;
-child = spawn("pwsh",["testps.ps1"]);
+child = spawn("pwsh",[process.env.GITHUB_ACTION +"/testps.ps1"]);
 child.stdout.on("data",function(data){
     process.stdout.write("" + data);
 });
 child.stderr.on("data",function(data){
     process.stdout.write("Powershell Errors: " + data);
+    process.exit(1);
 });
 child.on("exit",function(){
     process.stdout.write("Powershell Script finished");

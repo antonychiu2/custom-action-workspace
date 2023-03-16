5ea777686b75d7e0970bffe80682ef141399824c
Antony Chiu <chiu_antony@hcl.com>
2023-03-17 01:01:30 +0800

    test cancel

--- callps.js
+++ callps.js
@@ -1,10 +1,10 @@
 console.log(process.env)
-//process.env.GITHUB_ACTION_PATH = '/home/runner/work/_actions/antonychiu2/custom-action-workspace/main'
+var PSFileToRun = "testps.ps1";
 var GITHUB_ACTION_PATH = process.env.HOME+"/work/_actions/"+process.env.GITHUB_ACTION_REPOSITORY+"/"+process.env.GITHUB_ACTION_REF;
 console.log('Constructed github action path: '+GITHUB_ACTION_PATH)
 
 var spawn = require("child_process").spawn,child;
-child = spawn("pwsh",[GITHUB_ACTION_PATH+"/testps.ps1"]);
+child = spawn("pwsh",[GITHUB_ACTION_PATH+"/"+PSFileToRun]);
 child.stdout.on("data",function(data){
     process.stdout.write("" + data);
 });

b89db880e0909b9bc9d7def70eebb569e83a42ea
Antony Chiu <amtony@gmail.com>
2023-03-16 23:07:45 +0800

    v2

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log('This is the main workflow');
+
+async function sleep(ms){
+    return new Promise((resolve) => {
+        setTimeout(resolve,ms);
+    });
+}
+
+for(let i=0;i<60;i++){
+    await sleep(1000);
+    console.log('${i} minute has passed.');
+}

867b9fcadd7ae5363c78113d22a608c3182b1e53
Antony Chiu <chiu_antony@hcl.com>
2023-03-17 00:48:19 +0800

    test

--- callps.js
+++ callps.js
@@ -1,6 +1,6 @@
 console.log(process.env)
 //process.env.GITHUB_ACTION_PATH = '/home/runner/work/_actions/antonychiu2/custom-action-workspace/main'
-var GITHUB_ACTION_PATH = process.env.HOME+"/"+"/work/_actions/"+process.env.GITHUB_ACTION_REPOSITORY+"/"+process.env.GITHUB_ACTION_REF;
+var GITHUB_ACTION_PATH = process.env.HOME+"/work/_actions/"+process.env.GITHUB_ACTION_REPOSITORY+"/"+process.env.GITHUB_ACTION_REF;
 
 var spawn = require("child_process").spawn,child;
 child = spawn("pwsh",[GITHUB_ACTION_PATH+"/testps.ps1"]);
